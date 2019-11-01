cf23ddb78e0ab3b0973a8f6a020b6feec64cd05e
Ryan Liu <ryan@launchlab.com.au>
2019-11-01 15:07:09 +1100

    - Add eslint and its config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const webpack              = require('webpack')
 const merge                = require('webpack-merge')
@@ -7,35 +7,35 @@ const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const commonConfig         = require('./webpack.config.base')
 
 const webpackConfig = merge(commonConfig, {
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
-    output: {
-      path: helpers.root('dist'),
-        publicPath: '/',
-        filename: 'js/[name].bundle.js',
-        chunkFilename: 'js/[id].chunk.js'
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new FriendlyErrorsPlugin()
-    ],
-    devServer: {
-        compress: true,
-        historyApiFallback: true,
-        hot: true,
-        open: true,
-        overlay: true,
-        port: 8000,
-        stats: {
-            normal: true
-        }
-    }
-});
+	mode: 'development',
+	devtool: 'cheap-module-eval-source-map',
+	output: {
+		path: helpers.root('dist'),
+		publicPath: '/',
+		filename: 'js/[name].bundle.js',
+		chunkFilename: 'js/[id].chunk.js'
+	},
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new FriendlyErrorsPlugin()
+	],
+	devServer: {
+		compress: true,
+		historyApiFallback: true,
+		hot: true,
+		open: true,
+		overlay: true,
+		port: 8000,
+		stats: {
+			normal: true
+		}
+	}
+})
 
 module.exports = webpackConfig
